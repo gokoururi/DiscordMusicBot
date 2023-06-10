1307971bfd7b5c9572449cd4fcc7f411e8a1bec4
Gokou Ruri <asatier@mail.com>
2023-06-10 15:08:47 +0200

    Cleanup

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio
 
 import discord
-from discord.ext import commands, tasks
+from discord.ext import commands
 import os
 from dotenv import load_dotenv
 import youtube_dl
@@ -26,7 +26,7 @@ ytdlFormatOptions = {
     'quiet': False,
     'no_warnings': False,
     'default_search': 'auto',
-    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+    'source_address': '0.0.0.0'  # bind to ipv4 since ipv6 addresses cause issues sometimes
 }
 
 ffmpegOptions = {
@@ -34,6 +34,7 @@ ffmpegOptions = {
 }
 
 ytdl = youtube_dl.YoutubeDL(ytdlFormatOptions)
+
 
 class YTDLSource(discord.PCMVolumeTransformer):
     def __init__(self, source, *, data, volume=0.5):
@@ -51,6 +52,15 @@ class YTDLSource(discord.PCMVolumeTransformer):
         filename = data['title'] if stream else ytdl.prepare_filename(data)
         return filename
 
+
+def get_song_name(filename):
+    song = re.search('(.*)-.*', filename)
+    if song:
+        return re.sub('_', " ", song.group(1))
+    else:
+        return f"Wakarimasen! {filename}"
+
+
 @bot.command(name='join', help='Join channel')
 async def join(ctx):
     if not ctx.message.author.voice:
@@ -61,58 +71,55 @@ async def join(ctx):
     await channel.connect()
     await ctx.guild.change_voice_state(channel=channel, self_mute=False, self_deaf=True)
 
+
 @bot.command(name='leave', help='Leave channel')
 async def leave(ctx):
-    voiceClient = ctx.message.guild.voice_client
-    if voiceClient.is_connected():
-        await voiceClient.disconnect()
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_connected():
+        await voice_client.disconnect()
     else:
         await ctx.send("uwaaaah... s-something went wrong")
 
-def getSongName(filename):
-    song = re.search('(.*)-.*', filename)
-    if song:
-        return re.sub('_', " ", song.group(1))
-    else:
-        return f"Wakarimasen! {filename}"
 
 @bot.command(name='play')
 async def play(ctx, url):
     try:
         server = ctx.message.guild
-        voiceChannel = server.voice_client
+        voice_channel = server.voice_client
         async with ctx.typing():
             filename = await YTDLSource.from_url(url, loop=bot.loop)
-            voiceChannel.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source=filename))
-        await ctx.send(f"**p-playing** :sparkles:{getSongName(filename)}:sparkles:")
+            voice_channel.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source=filename))
+        await ctx.send(f"**p-playing** :sparkles:{get_song_name(filename)}:sparkles:")
     except Exception as err:
         await ctx.send("Not connected to voice channel")
         print(f"Error: {err=}")
 
+
 @bot.command(name='pause')
 async def pause(ctx):
-    voiceClient = ctx.message.guild.voice_client
-    if voiceClient.is_playing():
-        voiceClient.pause()
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing():
+        voice_client.pause()
     else:
         await ctx.send("I-I'm not playing anything r-right now.")
 
+
 @bot.command(name='resume')
-async def pause(ctx):
-    voiceClient = ctx.message.guild.voice_client
-    if voiceClient.is_paused():
-        voiceClient.resume()
+async def resume(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_paused():
+        voice_client.resume()
     else:
         await ctx.send("What d-do you mean 'resume'? I wasn't p-playing anything.")
 
+
 @bot.command(name='stop')
 async def stop(ctx):
-    voiceClient = ctx.message.guild.voice_client
-    if voiceClient.is_playing():
-        voiceClient.stop()
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing():
+        voice_client.stop()
     else:
         await ctx.send("s-stop? I wasn't doing anything!")
 
 if __name__ == '__main__':
     bot.run(DISCORDTOKEN)
-
